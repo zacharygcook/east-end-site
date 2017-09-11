66c88ae16b6f36cf0af8d9f667dbbc913d0ce9ad
Zach Cook <zach@zachcookhustles.com>
2017-09-11 00:43:54 -0500

    Contact form is COMPLETE!

--- js/auto-size-homepage-splash-area.js
+++ js/auto-size-homepage-splash-area.js
@@ -1,20 +1,29 @@
 $(document).ready(function(){
 
-	var windowHeight = $(window).height();
-	var newHeight = windowHeight - 80;
-	$('#splash-wrapper').css("height", newHeight);
+	if ($(window).height() > 800) {
+		var windowHeight = $(window).height();
+		var newHeight = windowHeight;
+		$('#splash-wrapper').css("height", newHeight);		
+	} else {
+		console.log("Too small for automatic splash page resizing.");
+	}
+	
 
 	$(window).on('resize', function() {
 		console.log("Browser got resized");
 
+		if ($(window).height() > 800) {
+			// Set height of splash page
+			console.log("Window height: ", $(window).height());
+			console.log("Window width: ", $(window).width());
+			var windowHeight = $(window).height();
+			var newHeight = windowHeight;
+			console.log("Now setting height to: ", newHeight);
+			$('#splash-wrapper').css("height", newHeight);	
+		} else {
+			console.log("Too small for automatic splash page resizing.");
+		}
 
-		// Set height of splash page
-		console.log("Window height: ", $(window).height());
-		console.log("Window width: ", $(window).width());
-		var windowHeight = $(window).height();
-		var newHeight = windowHeight - 80;
-		console.log("Now setting height to: ", newHeight);
-		$('#splash-wrapper').css("height", newHeight);
 	});
 
 });
