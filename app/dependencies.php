dbf0fd2534a8bb0dd0bdebcb44d3f00ca87503fe
Zach Cook <zach@zachcookhustles.com>
2017-09-12 20:02:59 -0500

    Added some basic emailing - successful.

--- app/dependencies.php
+++ app/dependencies.php
@@ -3,8 +3,6 @@
 // Get the container
 $container = $app->getContainer();
 
-
-
 // Twig view dependency
 $container['view'] = function ($c) {
 
@@ -17,3 +15,18 @@ $container['view'] = function ($c) {
 
     return $view;
 };
+
+
+// $container['mailer'] = function ($container) {
+// 	$mailer = new PHPMailer;
+
+// 	$mailer->Host = 'your.host.com';  // your email host, to test I use localhost and check emails using test mail server application (catches all  sent mails)
+// 	$mailer->SMTPAuth = true;                 // I set false for localhost
+// 	$mailer->SMTPSecure = 'ssl';              // set blank for localhost
+// 	$mailer->Port = your host smtp port;                           // 25 for local host
+// 	$mailer->Username = 'your@email.address';    // I set sender email in my mailer call
+// 	$mailer->Password = 'yourPassword';
+// 	$mailer->isHTML(true);
+
+// 	return new \App\Mail\Mailer($container->view, $mailer);
+// };
--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,8 @@
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Psr\Http\Message\ResponseInterface as Response;
 
+    use PHPMailer\PHPMailer\PHPMailer;
+
     // HOME ROUTE
     // Alternate way we can start doing routes if we want
     // $app->get('/', function (Request $request, Response $response, $args)   {
@@ -90,4 +92,113 @@
             ];
             return $this->view->render($response, 'quote-request.twig', $vars);    
         });
+
+        $this->get('email-test', function (Request $request, Response $response, $args) {
+            $vars = [
+                'page' => [
+                'title' => 'East End Ink',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+
+            $mail = new PHPMailer;
+
+
+            //Server settings
+            // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            // $mail->isSMTP();                                      // Set mailer to use SMTP
+            // $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+            // $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            // $mail->Username = 'user@example.com';                 // SMTP username
+            // $mail->Password = 'secret';                           // SMTP password
+            // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            // $mail->Port = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('from@example.com', 'Mailer');
+            $mail->addAddress('zach@zachcookhustles.com', 'Joe User');     // Add a recipient
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // Try to make it faster by not sending via SMTP???
+            // Got from this StackOverflow answer:
+            // https://stackoverflow.com/questions/27552252/sending-phpmailer-smtp-email-with-gmail-takes-long-time-1-5-seconds
+            $mail->IsMail();
+
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            if($mail->send()) {
+              echo "Message sent!";
+            } else {
+              echo "Mailer Error: " . $mail->ErrorInfo;
+            }
+
+            return $this->view->render($response, 'quote-request.twig', $vars);          
+        });
+
+        $this->post('home-contact-email', function (Request $request, Response $response, $args) {
+            $vars = [
+                'page' => [
+                'title' => 'East End Ink',
+                'description' => 'Best apparel company in Austin'
+                ],
+            ];
+
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
+
+            $mail = new PHPMailer;
+
+            //Server settings
+            // $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+            // $mail->isSMTP();                                      // Set mailer to use SMTP
+            // $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+            // $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            // $mail->Username = 'user@example.com';                 // SMTP username
+            // $mail->Password = 'secret';                           // SMTP password
+            // $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            // $mail->Port = 587;                                    // TCP port to connect to
+
+            //Recipients
+            $mail->setFrom('from@example.com', 'Mailer');
+            $mail->addAddress('zach@zachcookhustles.com', 'Joe User');     // Add a recipient
+            $mail->addReplyTo('info@example.com', 'Information');
+            $mail->addCC('cc@example.com');
+            $mail->addBCC('bcc@example.com');
+
+            //Attachments
+            // $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+            // Try to make it faster by not sending via SMTP???
+            // Got from this StackOverflow answer:
+            // https://stackoverflow.com/questions/27552252/sending-phpmailer-smtp-email-with-gmail-takes-long-time-1-5-seconds
+            $mail->IsMail();
+            
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = 'Here is the subject';
+            $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+            $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+            if($mail->send()) {
+              echo "Message sent!";
+            } else {
+              echo "Mailer Error: " . $mail->ErrorInfo;
+            }
+
+            return $this->view->render($response, 'quote-request.twig', $vars);          
+        });
+
     });
