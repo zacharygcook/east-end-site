caef8e4d3fab986dc7a158c3c57fecffaa86d673
Zach Cook <zach@zachcookhustles.com>
2017-09-12 22:02:43 -0500

    Tiny update.

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,5 @@
 <?php
 
-    // Creating routes
-
     // Psr-7 Request and Response interfaces
     use Psr\Http\Message\ServerRequestInterface as Request;
     use Psr\Http\Message\ResponseInterface as Response;
