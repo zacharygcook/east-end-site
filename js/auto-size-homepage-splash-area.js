bcca10d8d9d555f782d52b1c4d35fcc7fb941e7a
Zach Cook <zach@zachcookhustles.com>
2017-09-19 13:27:39 -0500

    Should be a better splash page.

--- js/auto-size-homepage-splash-area.js
+++ js/auto-size-homepage-splash-area.js
@@ -1,29 +1,18 @@
 $(document).ready(function(){
 
-	if ($(window).height() > 800) {
-		var windowHeight = $(window).height();
-		var newHeight = windowHeight;
-		$('#splash-wrapper').css("height", newHeight);		
-	} else {
-		console.log("Too small for automatic splash page resizing.");
-	}
-	
+	var windowHeight = $(window).height();
+	var newHeight = windowHeight;
+	$('#splash-wrapper').css("height", newHeight);
 
 	$(window).on('resize', function() {
 		console.log("Browser got resized");
 
-		if ($(window).height() > 800) {
-			// Set height of splash page
-			console.log("Window height: ", $(window).height());
-			console.log("Window width: ", $(window).width());
-			var windowHeight = $(window).height();
-			var newHeight = windowHeight;
-			console.log("Now setting height to: ", newHeight);
-			$('#splash-wrapper').css("height", newHeight);	
-		} else {
-			console.log("Too small for automatic splash page resizing.");
-		}
-
+		console.log("Window height: ", $(window).height());
+		console.log("Window width: ", $(window).width());
+		var windowHeight = $(window).height();
+		var newHeight = windowHeight;
+		console.log("Now setting height to: ", newHeight);
+		$('#splash-wrapper').css("height", newHeight);
 	});
 
 });
