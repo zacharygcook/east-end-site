4806ccb83c99e3336df0023f9adf214b48a39c29
Zach Cook <zach@zachcookhustles.com>
2017-09-20 14:31:47 -0500

    Sending email works, its awesome.

--- app/routes.php
+++ app/routes.php
@@ -132,6 +132,19 @@
         });
 
         $this->post('drop-down-contact-form', function (Request $request, Response $response, $args) {
+            error_log("The right endpoint did get hit.", 3, "/var/www/html/error_logs/EEI_errors.log");
+
+            $mail = new PHPMailer;
+
+            $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+            $mail->isSMTP();                                      // Set mailer to use SMTP
+            $mail->Host = 'mail.smtp2go.com';                    // Specify main and backup SMTP servers
+            $mail->SMTPAuth = true;                               // Enable SMTP authentication
+            $mail->Username = 'zach@zachcookhustles.com';                 // SMTP username
+            $mail->Password = 'w9xAzaSNeXYa';                          // SMTP password
+            $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+            $mail->Port = 2525;                                    // TCP port to connect to
+
             // Get the form fields and remove whitespace.
             $name = strip_tags(trim($_POST["name"]));
                     $name = str_replace(array("\r","\n"),array(" "," "),$name);
@@ -139,38 +152,41 @@
             $message = trim($_POST["message"]);
 
             // Check that data was sent to the mailer.
-            if ( empty($name) OR empty($message)) {
+            if (empty($name) OR empty($message)) {
                 // Set a 400 (bad request) response code and exit.
                 http_response_code(400);
                 echo "Oops! There was a problem with your submission. Please complete the form and try again.";
                 exit;
             }
 
-            // Set the recipient email address.
-            // FIXME: Update this to your desired email address.
-            $recipient = "zach@zachcookhustles.com";
-
             // Set the email subject.
-            $subject = "New contact from $name";
+            $subject = "New contact Us: from $name";
 
             // Build the email content.
             $email_content = "Name: $name\n";
             $email_content .= "Email: $email\n\n";
             $email_content .= "Message:\n$message\n";
 
-            // Build the email headers.
-            $email_headers = "From: $name <$email>";
+            //Recipients
+            $mail->setFrom("$email", "$name");
+            $mail->addAddress('zach@zachcookhustles.com', 'Zachary Cook');     // Add a recipient
+            $mail->addReplyTo("$email", "$name");
+            
+            //Content
+            $mail->isHTML(true);                                  // Set email format to HTML
+            $mail->Subject = "$subject";
+            $mail->Body    = "$email_content";
 
-            // Send the email.
-            if (mail($recipient, $subject, $email_content, $email_headers)) {
-                // Set a 200 (okay) response code.
-                http_response_code(200);
-                echo "Thank You! Your message has been sent.";
+            if($mail->send()) {
+                $responseToSend = $response->withStatus(200);
             } else {
-                // Set a 500 (internal server error) response code.
-                http_response_code(500);
-                echo "Oops! Something went wrong and we couldn't send your message.";
+                $responseToSend = $response->withStatus(404);
+                error_log("Mailer Error: " . $mail->ErrorInfo, 3, "/var/www/html/error_logs/EEI_errors.log");
             }
+
+            error_log("Response we're sending: ". $responseToSend, 3, "/var/www/html/error_logs/EEI_errors.log");
+
+            return $responseToSend;  
 
         });
 
