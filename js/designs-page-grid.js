ebb71542d677acf4d34ea832c4221c6221d152e6
Zach Cook <zach@zachcookhustles.com>
2017-09-23 22:05:00 -0500

    Grid system is DONE. No mas.

--- js/designs-page-grid.js
+++ js/designs-page-grid.js
@@ -18,10 +18,19 @@ $(function() {
     if ($(this).css("position") == "relative") {
      
       $(this).attr('id', 'active-img-container');
-      $(this).css({"transform": "none !important"});
+      $(this).css({"padding-top": "135px"});
+
+      $(this).parent().prepend("<div id='replace-missing-container-div'></div>");
+
+      // Change the image's styling
+      var newImgHeight = $(window).height() * 0.7;
+      $(this).find("img").css({ "height": newImgHeight, "width": "auto", "position": "relative"});
 
     } else {
       $(this).attr('id', '');
+      $(this).css({"transform": "none !important", "padding-top": "0"});
+      $(this).find("img").css({ "height": "auto", "width": "100%", "position": "absolute"});
+      $(this).parent().parent().find("#replace-missing-container-div").remove();
     }
     
   });  
